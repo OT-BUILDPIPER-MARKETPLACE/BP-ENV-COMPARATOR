234caa05c29746704c58df84d7cf05ebf3d2e92f
deepak.gupta <deepak.gupta@opstree.com>
2024-02-13 18:36:00 +0530

    Update the configmap function

--- build.sh
+++ build.sh
@@ -20,22 +20,22 @@ sleep  $SLEEP_DURATION
 
 TASK_STATUS=0
 
-logInfoMessage "Service ${SERVICE_NAME} ConfigMap in namespace : ${SOURCE_NAMESPACE} comparison to target namespaces : ${CM_NAMESPACES}"
+logInfoMessage "Will do ConfigMap Comparison for Service ${SERVICE_NAME} in Source ENVIRONMENT: ${SOURCE_NAMESPACE} to target ENVIRONMENT: ${CM_NAMESPACES}"
 
 serviceExists ${SERVICE_NAME} ${SOURCE_NAMESPACE}
 if [ "$?" -eq 0  ]; then
 	CONFLICTING_KEYS=`compareKeyCount ${SERVICE_NAME} ${SOURCE_NAMESPACE} ${CM_NAMESPACES}`
 	if [ -z "${CONFLICTING_KEYS}" ]; then
 		TASK_STATUS=0
-            logInfoMessage "Keys are ideal for all namespaces for ${SERVICE_NAME}"
+            logInfoMessage "Keys are ideal for all Environment for ${SERVICE_NAME}"
 	else
           TASK_STATUS=1
-          logInfoMessage "Listing out the conflicting keys for ${SERVICE_NAME} in all namespaces"
+          logInfoMessage "Listing out the conflicting keys for ${SERVICE_NAME} in all Environments"
 	  conflictkeys ${SERVICE_NAME} ${SOURCE_NAMESPACE} ${CM_NAMESPACES}
 	fi
    else
      TASK_STATUS=1
-     logErrorMessage "Service ${SERVICE_NAME} does not exist in the given namespace ${SOURCE_NAMESPACE}"
+     logErrorMessage "Service ${SERVICE_NAME} does not exist in the given Environment ${SOURCE_NAMESPACE}"
 fi
 
 
